d551e08142d4bdbf59793aac2432a3fcaf7954dc
icaihui-1987 <caihui3209@163.com>
2020-08-09 21:24:51 +0800

    master

--- 10.泛型、trait与生命周期/learn_life2/target/rls/debug/deps/learn_life2-fa816e93ed265c4f.d
+++ 10.泛型、trait与生命周期/learn_life2/target/rls/debug/deps/learn_life2-fa816e93ed265c4f.d
@@ -1,5 +1,5 @@
-d:\VSCode-Rust\10.泛型、trait与生命周期\learn_life2\target\rls\debug\deps\learn_life2-fa816e93ed265c4f.rmeta: src\main.rs
+e:\学习Rust\10.泛型、trait与生命周期\learn_life2\target\rls\debug\deps\learn_life2-fa816e93ed265c4f.rmeta: src\main.rs
 
-d:\VSCode-Rust\10.泛型、trait与生命周期\learn_life2\target\rls\debug\deps\learn_life2-fa816e93ed265c4f.d: src\main.rs
+e:\学习Rust\10.泛型、trait与生命周期\learn_life2\target\rls\debug\deps\learn_life2-fa816e93ed265c4f.d: src\main.rs
 
 src\main.rs:
